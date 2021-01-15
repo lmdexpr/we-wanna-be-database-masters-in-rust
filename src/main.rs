778dbcc191241e1e32f0c5423769508b32b365d8
cw-tajiri <lmdexpr@gmail.com>
2021-01-15 20:26:32 +0900

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
 use std::io::{stdin, stdout, Write};
 
-fn main() {
-    loop {
-        print!("db > ");
-        stdout().flush().unwrap();
+enum StatementType {
+    StatementInsert,
+    StatementSelect,
+}
 
-        let mut line = String::new();
+struct Statement {
+    s_type: StatementType,
+}
 
-        stdin().read_line(&mut line).unwrap();
-
-        match &*line.trim() {
-            ".exit" => std::process::exit(0),
-            trimed => println!("Unrecognized command {}", trimed)
-        }
+fn execute_meta(cmd: &str) -> Result<(), String> {
+    match cmd {
+        ".exit" => std::process::exit(0),
+        _       => Err(format!("Unrecognized command '{}'", cmd)),
     }
+}
+
+fn parse(raw: &str) -> Result<Statement, String> {
+    match raw {
+        raw if raw.starts_with("insert") =>
+            Ok(Statement { s_type : StatementType::StatementInsert }),
+        "select" => Ok(Statement { s_type : StatementType::StatementSelect }),
+        raw      => Err(format!("Unrecognized keyword at start of '{}'", raw)),
+    }
+}
+
+fn execute(statement: Statement) {
+    match statement.s_type {
+        StatementType::StatementInsert => println!("This is where we would do an insert."),
+        StatementType::StatementSelect => println!("This is where we would do an select."),
+    }
+
+    println!("Executed.")
+}
+
+fn interpreter() {
+    print!("db > ");
+    stdout().flush().unwrap();
+
+    let line = {
+        let mut line = String::new();
+        stdin().read_line(&mut line).unwrap();
+        &*line.trim().to_owned()
+    };
+
+    if line.starts_with(".") {
+        execute_meta(line)
+    } else {
+        parse(line).map(execute)
+    }
+    .unwrap_or_else(|msg| { println!("{}", msg) })
+}
+
+fn main() {
+    loop { interpreter() }
 }
